c4653df2a22679f0df00ccffd14ca9b57611331e
Kevin Wang <kevin-s-wang@users.noreply.github.com>
2023-07-25 14:10:38 +0800

    Update adapter_v2.py

--- finetune/adapter_v2.py
+++ finetune/adapter_v2.py
@@ -34,8 +34,8 @@ override_max_seq_length = None
 
 # Hyperparameters
 learning_rate = 3e-3
-batch_size = 64 / devices
-micro_batch_size = 1  # set to 2 because this is fit into 12GB Vram
+batch_size = 128 / devices
+micro_batch_size = 2  # set to 2 because this is fit into 12GB Vram
 gradient_accumulation_iters = batch_size // micro_batch_size
 assert gradient_accumulation_iters > 0
 epoch_size = 50000  # train dataset size
