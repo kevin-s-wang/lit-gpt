9fa8af1608888639348a3ea02138c293d9ad4fb1
Kevin Wang <kevin-s-wang@users.noreply.github.com>
2023-07-25 13:55:24 +0800

    Update adapter_v2.py

--- finetune/adapter_v2.py
+++ finetune/adapter_v2.py
@@ -30,7 +30,7 @@ eval_iters = 100
 log_interval = 1
 devices = 1
 # change this value to force a maximum sequence length
-override_max_seq_length = 100
+override_max_seq_length = None
 
 # Hyperparameters
 learning_rate = 3e-3
